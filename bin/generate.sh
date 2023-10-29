e09907499c9a0364557bf9116bff9e7b4284367d
Kerry Johnson <kerry.t.johnson@gmail.com>
2023-10-29 15:21:50 -0600

    feat: initial

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
 
-for lang in typescript-angular python-flask; do
+
+for lang in typescript-angular python-flask python-aiohttp; do
+    if [[ -d ${PWD}/out/${lang} ]]; then
+        rm -rf ${PWD}/out/${lang}/*.*
+    fi
+
     docker run  --rm                                                        \
                 --volume "${PWD}:/local"                                    \
                 --user 1000:1000                                            \
+                "${@}"                                                      \
                 openapitools/openapi-generator-cli:v6.6.0                   \
                     generate    --generator-name    ${lang}                 \
                                 --output            /local/out/${lang}      \
-                                --model-package     boids                   \
+                                --model-package     model                   \
                                 --package-name      boidsapi                \
                                 --input-spec        /local/openapi.yaml
+    echo "Generated ${lang}"
 done
